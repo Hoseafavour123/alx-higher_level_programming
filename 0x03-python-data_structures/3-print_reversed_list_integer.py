b193be22e7a26546fc33ce3c36edca21f597a085
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-12-20 22:48:47 +0000

    Some debugging

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,8 @@
 
 def print_reversed_list_integer(my_list=[]):
     """Print a list of integers in reverse"""
+    if (len(my_list == 0)):
+        pass
     i = len(my_list) - 1
 
     for j in range(len(my_list) - 1):
