72640add1eee8c1587dfd82d6e681982a5da97f2
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-12-20 22:57:17 +0000

    Debugging data structures

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,13 +5,14 @@ def print_reversed_list_integer(my_list=[]):
     if not my_list:
         pass
 
-    i = len(my_list) - 1
+    else:
+        i = len(my_list) - 1
 
-    for j in range(len(my_list) - 1):
-        if (j < i):
-            tmp = my_list[j]
-            my_list[j] = my_list[i]
-            my_list[i] = tmp
-        i -= 1
-    for num in my_list:
-        print("{:d}".format(num))
+        for j in range(len(my_list) - 1):
+            if (j < i):
+                tmp = my_list[j]
+                my_list[j] = my_list[i]
+                my_list[i] = tmp
+            i -= 1
+        for num in my_list:
+            print("{:d}".format(num))
