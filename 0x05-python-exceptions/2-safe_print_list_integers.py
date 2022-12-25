a81be08a6bda31e4628a83d30984a0d528ecedb7
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-12-25 02:25:12 +0000

    Cleaning up bugs

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except (ValueError, TypeError, IndexError):
+        except (ValueError, TypeError):
             pass
         else:
             count += 1
