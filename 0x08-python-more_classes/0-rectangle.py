9a82e3bfe487f127a5cb29dd2efd06f8eb94946e
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-01-04 23:54:32 +0000

    Debugging

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""An empty class that defines a rectangle"""
+"""An empty class that defines a rectangle
+"""
 
 
 class Rectangle():
+    """An empty class"""
     pass
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -51,4 +51,7 @@ class Rectangle:
 
     def perimeter(self):
         """computes perimeter of rectangle"""
-        return (2 * (self.__width + self.__height))
+
+        if (self.__width == 0) or (self.__height == 0):
+            return 0
+        return ((self.__width * 2) + (self.__height * 2))
