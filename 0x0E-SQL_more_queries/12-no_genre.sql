fe1582257f7b4b346ede2a50401f6c44d3ae51c3
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-02-21 20:09:50 +0000

    Hot fix

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,7 @@
 -- Displays shows with no genre
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows LEFT JOIN tv_show_genres
-WHERE tv_show_genres.genre_id is NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+FROM tv_show_genres
+RIGHT JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
