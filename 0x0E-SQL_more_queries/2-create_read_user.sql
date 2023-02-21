b36d8cd7ab71ff39b2d853a675197c6216724e2f
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-02-21 20:16:32 +0000

    Hot fix

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,6 @@
 -- Create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create user
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- Grant SELECT privilege
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'loccalhost';
-FLUSH PRIVILEGES;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@loccalhost;
